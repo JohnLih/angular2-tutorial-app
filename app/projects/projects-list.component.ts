c7effc93d60f658c88a3597d99703070c7c0b63e
Shripal Soni <shripalsoni04@gmail.com>
2016-03-19 21:21:49 +0530

    updating project list dummy data.

--- app/projects/projects-list.component.ts
+++ app/projects/projects-list.component.ts
@@ -33,54 +33,78 @@ export class ProjectListComponent{
         this.projects = [{
             id: 1,
             name: 'Project 1',
-            clientName: 'Client 1',
+            client: {
+                id: 1,
+                name: 'Client 1',
+                timezon: 'PST',
+                country: 'USA',
+                email: 'client1@domain.com'
+            },
             details: 'Project 1 Description',
             isCompleted: false,
             startDate: 1451606400000,
             endDate: 1456770600000, 
             employees: [
-                {id: 1, name: 'Employee 1', role: 'FrontEnd Developer'},
-                {id: 2, name: 'Employee 2', role: 'BackEnd Developer'},
-                {id: 3, name: 'Employee 3', role: 'Tester'},
-                {id: 4, name: 'Employee 4', role: 'Project Manager'}
+                {id: 1, name: 'Employee 1', role: 'FrontEnd Developer', email: 'emp1@domain.com'},
+                {id: 2, name: 'Employee 2', role: 'BackEnd Developer', email: 'emp2@domain.com'},
+                {id: 3, name: 'Employee 3', role: 'Tester', email: 'emp3@domain.com'},
+                {id: 4, name: 'Employee 4', role: 'Project Manager', email: 'emp4@domain.com'}
             ]
         },{
             id: 2,
             name: 'Project 2',
-            clientName: 'Client 2',
+            client: {
+                id: 2,
+                name: 'Client 2',
+                timezon: 'IST',
+                country: 'India',
+                email: 'client2@domain.com'
+            },
             details: 'Project 2 Description',
             isCompleted: true,
             startDate: 1433097000000,
             endDate: 1436898600000, 
             employees: [
-                {id: 4, name: 'Employee 4', role: 'Project Manager'},
-                {id: 5, name: 'Employee 5', role: 'FullStack Developer'}
+                {id: 4, name: 'Employee 4', role: 'Project Manager', email: 'emp4@domain.com'},
+                {id: 5, name: 'Employee 5', role: 'FullStack Developer', email: 'emp5@domain.com'}
             ]
         },{
             id: 3,
             name: 'Project 3',
-            clientName: 'Client 3',
+            client: {
+                id: 3,
+                name: 'Client 3',
+                timezon: 'CET',
+                country: 'Germany',
+                email: 'client3@domain.com'
+            },
             details: 'Project 3 Description',
             isCompleted: true,
             startDate: 1438367400000,
             endDate: 1444415400000, 
             employees: [
-                {id: 2, name: 'Employee 2', role: 'BackEnd Developer'},
-                {id: 3, name: 'Employee 3', role: 'Tester'},
-                {id: 4, name: 'Employee 4', role: 'Project Manager'}
+                {id: 2, name: 'Employee 2', role: 'BackEnd Developer', email: 'emp2@domain.com'},
+                {id: 3, name: 'Employee 3', role: 'Tester', email: 'emp3@domain.com'},
+                {id: 4, name: 'Employee 4', role: 'Project Manager', email: 'emp4@domain.com'}
             ]
         },{
             id: 4,
             name: 'Project 4',
-            clientName: 'Client 4',
+            client: {
+                id: 4,
+                name: 'Client 4',
+                timezon: 'AEST',
+                country: 'Austrilia',
+                email: 'client4@domain.com'
+            },
             details: 'Project 4 Description',
             isCompleted: false,
             startDate: 1456770600000,
             endDate: 1464719400000, 
             employees: [
-                {id: 1, name: 'Employee 1', role: 'FrontEnd Developer'},
-                {id: 3, name: 'Employee 3', role: 'Tester'},
-                {id: 4, name: 'Employee 4', role: 'Project Manager'}
+                {id: 1, name: 'Employee 1', role: 'FrontEnd Developer', email: 'emp1@domain.com'},
+                {id: 3, name: 'Employee 3', role: 'Tester', email: 'emp3@domain.com'},
+                {id: 4, name: 'Employee 4', role: 'Project Manager', email: 'emp4@domain.com'}
             ]
         }];    
     }
